5e2a14c87d31ab91a80d729fa2c4224db742ac11
Angelo Biscola <angelo.biscola@gmail.com>
2016-05-25 09:51:03 -0300

    update LOG

--- resources/views/admin/log/log.blade.php
+++ resources/views/admin/log/log.blade.php
@@ -1,48 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel log viewer</title>
+@extends('layout.app')
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+@section('css')
+    @parent
     <link rel="stylesheet" href="https://cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.css">
+@stop
 
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <style>
-        body {
-            padding: 25px;
-        }
-        h1 {
-            font-size: 1.5em;
-            margin-top: 0px;
-        }
-        .stack {
-            font-size: 0.85em;
-        }
-        .date {
-            min-width: 75px;
-        }
-        .text {
-            word-break: break-all;
-        }
-        a.llv-active {
-            z-index: 2;
-            background-color: #f5f5f5;
-            border-color: #777;
-        }
-    </style>
-</head>
-<body>
+@section('content')
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
@@ -96,6 +59,10 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+@parent
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
@@ -118,9 +85,8 @@
             $('#' + $(this).data('display')).toggle();
         });
         $('#delete-log').click(function(){
-            return confirm('Are you sure?');
+            return confirm('Confirma a Exclusão?');
         });
     });
 </script>
-</body>
-</html>
+@stop
